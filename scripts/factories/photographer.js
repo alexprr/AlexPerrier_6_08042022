de798ad016c4cb9db906aebdab306477b21dadef
Alex <alexperrier.pro@gmail.com>
2022-04-08 09:43:12 +0200

    Add homepage photographers infos

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,31 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        // Création de l'article
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
+        // Création des paragraphes
+        const photographerCity = document.createElement('p');
+        photographerCity.classList.add("photographer-city");
+        const photographerTagline = document.createElement('p');
+        photographerTagline.classList.add("photographer-tagline");
+        const photographerPrice = document.createElement('p');
+        photographerPrice.classList.add("photographer-price");
+        photographerCity.textContent = `${city}, ${country}`;
+        photographerTagline.textContent = tagline;
+        photographerPrice.textContent = price + "/jour";
+        article.appendChild(photographerCity);
+        article.appendChild(photographerTagline);
+        article.appendChild(photographerPrice);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, tagline, price, getUserCardDOM }
 }
