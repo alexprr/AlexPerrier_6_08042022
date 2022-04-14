a576e70ed61948d089d75daea911ced9ebb394b0
Alex <alexperrier.pro@gmail.com>
2022-04-14 12:33:25 +0200

    Display data based on photographer's id

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,26 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographers() {
+    const photographers = fetch("../data/photographers.json")
+        .then((res) => res.json()) 
+        .then((photographers) => photographers)
+        .catch((err) => console.log("Failed to load photographers data" + err));
+
+    return photographers;
+}
+
+async function displayPhotographersPage() {
+    const {photographers} = await getPhotographers();
+    const params = new URLSearchParams(document.location.search);
+    const photographId = params.get("id");
+    const selectedPhotographer = photographers.find(
+        (photographer) => photographer.id == photographId)
+    const photographerHeader = document.querySelector(".photograph-header");
+    photographerHeader.innerHTML += new Photographer(selectedPhotographer).userPageHeader;
+}
+
+
+
+async function init() {
+    await displayPhotographersPage();
+};
+
+init();
