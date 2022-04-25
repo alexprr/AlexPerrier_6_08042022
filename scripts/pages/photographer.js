64c164bec1a3e2ef02d51292532aa1bed1ad5c35
Alex <alexperrier.pro@gmail.com>
2022-04-25 08:48:02 +0200

    Gallerie : Système de tri

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -7,7 +7,28 @@ async function getPhotographers() {
     return photographers;
 }
 
+// Filtres 
+const mediaFilter = (mediaGallery, option) => {
+    switch(option) {
+        case "popularity" : 
+            return mediaGallery.sort((a,b) => b.likes - a.likes);
+        case "date" : 
+            return mediaGallery.sort((a,b) => new Date(b.date) - new Date(a.date));
+        case "title" :
+            return mediaGallery.sort((a,b) => a.title.localCompare(b.title));
+    }
+}; 
 
+const updateGallery = (gallery) => {
+    gallery.forEach((media) => {
+        const allMedias = new MediaFactory(media)
+		$sectionMedia.innerHTML += allMedias.mediaGallery;
+	});
+}
+
+const $photographerHeader = document.querySelector(".photograph-header");
+const $photographerFooter = document.querySelector("#sticky-footer");
+const $sectionMedia = document.querySelector("#section-media");
 
 async function displayPhotographersPage() {
     const {photographers, media} = await getPhotographers();
@@ -17,29 +38,32 @@ async function displayPhotographersPage() {
         (photographer) => photographer.id == photographId)
         
     // Profil Header
-    const $photographerHeader = document.querySelector(".photograph-header");
     $photographerHeader.innerHTML += new Photographer(selectedPhotographer).userPageHeader;
 
-    // Profil Footer
-    // const $photographerFooter = document.querySelector(".photograph-footer");
-    // $photographerFooter.innerHTML += new Photographer(selectedPhotographer).userPageFooter;
+    // Sticky Footer
+    $photographerFooter.innerHTML += new Photographer(selectedPhotographer).userPageFooter;
 
     // Gallery
     const mediaGallery = media.filter((media) => media.photographerId == photographId);
-    mediaGallery.forEach((media) => {
-        const allMedias = new MediaFactory(media)
-        const $sectionMedia = document.querySelector("#section-media");
-		$sectionMedia.innerHTML += allMedias.mediaGallery;
-	});
+    updateGallery(mediaGallery);
+
+    // Filtered Gallery
+    document.addEventListener("change", (e) => {
+        $sectionMedia.innerHTML = "";
+        const option = mediaFilter(mediaGallery, e.target.value);
+        updateGallery(option)
+    })
 
     // Lightbox
     const lightbox = new Lightbox(mediaGallery);
-    document.querySelectorAll("#section-media .gallery-card").forEach(galleryCard => {
+    document.querySelectorAll("#section-media .gallery-img").forEach(galleryCard => {
         galleryCard.addEventListener("click", (e) => {
             lightbox.show(e.currentTarget.dataset.id);
         })
     })
 }
+
+
 
 
 async function init() {
