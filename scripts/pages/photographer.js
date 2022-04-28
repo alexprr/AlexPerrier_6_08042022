de827aa8e86864f5bc0a4c55d0b2d2d8f02991de
Alex <alexperrier.pro@gmail.com>
2022-04-28 14:51:15 +0200

    fix: Projet Errors

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -8,22 +8,11 @@ async function getPhotographers() {
     return photographers;
 }
 
-<<<<<<< HEAD
 // DOM Elements
 const $photographerHeader = document.querySelector(".photograph-header");
 const $photographerFooter = document.querySelector("#sticky-footer");
 const $sectionMedia = document.querySelector("#section-media");
 
-// Filtres 
-const mediaFilter = (mediaGallery, option) => {
-    switch(option) {
-        case "popularity" : 
-            return mediaGallery.sort((a,b) => b.likes - a.likes);
-        case "date" : 
-            return mediaGallery.sort((a,b) => new Date(b.date) - new Date(a.date));
-    }
-}; 
-=======
 // Filtres 
 const mediaFilter = (mediaGallery, option) => {
     switch(option) {
@@ -43,11 +32,6 @@ const updateGallery = (gallery) => {
 	});
 }
 
-const $photographerHeader = document.querySelector(".photograph-header");
-const $photographerFooter = document.querySelector("#sticky-footer");
-const $sectionMedia = document.querySelector("#section-media");
->>>>>>> 64c164bec1a3e2ef02d51292532aa1bed1ad5c35
-
 async function displayPhotographersPage() {
     const {photographers, media} = await getPhotographers();
     const params = new URLSearchParams(document.location.search);
@@ -55,28 +39,18 @@ async function displayPhotographersPage() {
     const selectedPhotographer = photographers.find(
         (photographer) => photographer.id == photographId)
         
-<<<<<<< HEAD
     // Account Header
     $photographerHeader.innerHTML += new Photographer(selectedPhotographer).userPageHeader;
 
-    // Account Sticky Footer
-=======
-    // Profil Header
-    $photographerHeader.innerHTML += new Photographer(selectedPhotographer).userPageHeader;
-
-    // Sticky Footer
->>>>>>> 64c164bec1a3e2ef02d51292532aa1bed1ad5c35
+    // Account Footer
     $photographerFooter.innerHTML += new Photographer(selectedPhotographer).userPageFooter;
 
     // Account Gallery
     const mediaGallery = media.filter((media) => media.photographerId == photographId);
-<<<<<<< HEAD
     mediaGallery.forEach((media) => {
         const allMedias = new MediaFactory(media)
 		$sectionMedia.innerHTML += allMedias.mediaGallery;
 	});
-=======
-    updateGallery(mediaGallery);
 
     // Filtered Gallery
     document.addEventListener("change", (e) => {
@@ -84,7 +58,6 @@ async function displayPhotographersPage() {
         const option = mediaFilter(mediaGallery, e.target.value);
         updateGallery(option)
     })
->>>>>>> 64c164bec1a3e2ef02d51292532aa1bed1ad5c35
 
     // Lightbox
     const lightbox = new Lightbox(mediaGallery);
@@ -93,14 +66,8 @@ async function displayPhotographersPage() {
             lightbox.show(e.currentTarget.dataset.id);
         })
     })
-
-    // Likes
-    getLikes();
 }
     
-
-
-
 async function init() {
     await displayPhotographersPage();
 };
